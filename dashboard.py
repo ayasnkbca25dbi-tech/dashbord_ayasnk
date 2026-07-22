7dfe4b686adc913ea63165ca658d6242e5ece27e
AYAS FAYAS <ayasnk.bca25.dbi@gmail.com>
2026-07-22 15:07:43 +0530

    Fix CSV file path

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("data/gitnovamart_clean.csv")
+    return pd.read_csv("data/novamart_clean.csv")
 
 df = load_data()
 st.title("NovaMart Dashboard - Ayas nk")
